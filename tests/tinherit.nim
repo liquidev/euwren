76b8a7d3d88b875a5526964458e5987064435f06
liquid600pgm <liquidekgaming@gmail.com>
2019-11-16 21:54:01 +0100

    Inheritance type check test

---
+++ tests/tinherit.nim
@@ -0,0 +1,67 @@
+import euwren
+
+type
+  A = ref object of RootObj
+    x: int
+  B = ref object of A
+    y: int
+  C = ref object
+    z: int
+
+var wren = newWren()
+
+proc newA(x: int): A = A(x: x)
+proc newB(x, y: int): B = B(x: x, y: y)
+proc newC(z: int): C = C(z: z)
+
+proc printA(a: A) =
+  echo "success!"
+  echo a.x
+
+proc printC(c: C) =
+  echo "fail!"
+  echo c.z
+
+wren.foreign("inherit"):
+  A:
+    [new] newA
+  B:
+    [new] newB
+  C:
+    [new] newC
+  Test:
+    printA
+    printC
+  module """
+    foreign class A {
+      construct new(x) {}
+    }
+    foreign class B {
+      construct new(x, y) {}
+    }
+    foreign class C {
+      construct new(z) {}
+    }
+    class Test {
+      foreign static printA(a)
+      foreign static printC(c)
+    }
+  """
+
+wren.run("""
+import "inherit" for A, B, C, Test
+
+var a = A.new(1)
+var b = B.new(a, 3)
+
+Test.printA(b)
+
+var error = Fiber.new {
+  var a = A.new(1)
+  Test.printC(a)
+}.try()
+if (error) {
+  System.print("success!")
+  System.print("error message: " + error)
+}
+""")
