f40b766bab7413d1cd602244b9fef34c5d9401e8
liquid600pgm <liquidekgaming@gmail.com>
2019-11-23 15:58:48 +0100

    Made the nimterop wrapper not use getHeader beacuse debug info is important.

--- src/euwren/private/wren.nim
+++ src/euwren/private/wren.nim
@@ -1,13 +1,33 @@
+import os
+
 import nimterop/build
 import nimterop/cimport
 
 const
   Base = getProjectCacheDir("euwren")
+  Src = Base/"src"
+  Vm = Src/"vm"
+  Optional = Src/"optional"
+  Include = Src/"include"
 
-setDefines(@["wrenStatic", "wrenGit", "wrenSetVer=0.2.0"])
+static:
+  gitPull("https://github.com/wren-lang/wren.git", Base, """
+src/vm/*
+src/optional/*
+src/include/*""")
 
-getHeader("src/include/wren.h",
-          gitUrl = "https://github.com/wren-lang/wren.git",
-          outdir = Base)
+cIncludeDir(Include)
+cIncludeDir(Vm)
+cIncludeDir(Optional)
 
-cImport(wrenPath)
+cCompile(Vm/"wren_compiler.c")
+cCompile(Vm/"wren_core.c")
+cCompile(Vm/"wren_debug.c")
+cCompile(Vm/"wren_primitive.c")
+cCompile(Vm/"wren_utils.c")
+cCompile(Vm/"wren_value.c")
+cCompile(Vm/"wren_vm.c")
+cCompile(Optional/"wren_opt_meta.c")
+cCompile(Optional/"wren_opt_random.c")
+
+cImport(Include/"wren.h")
